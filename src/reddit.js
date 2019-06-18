5e4685310c9104ee2f770d96c03cfef2a8fb1efe
Vinh Tu <vinhu125@gmail.com>
2019-06-18 11:11:57 +0700

    commit commit

--- src/reddit.js
+++ src/reddit.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
+import moment from 'moment';
 export default class Reddit extends Component {
   constructor(props) {
     super(props);
@@ -12,12 +13,10 @@ export default class Reddit extends Component {
   componentDidMount() {
     try {
       axios.get('https://www.reddit.com/hot/.json').then(res => {
-        var arr = [];
-
         this.setState({
-          post: res.data.data.children,
+          post: res.data.data.children.slice(0, 10),
         });
-        // arr.push(this.state.post.title);
+        console.log(res.data.data.children.slice(0, 10), 'array 8 ele');
       });
     } catch (e) {
       console.log(e.massage);
@@ -31,13 +30,22 @@ export default class Reddit extends Component {
           <div className="name">
             <p>Call API from reddit </p>
           </div>
+
           {this.state.post.map((p, idx) => {
-            console.log(p, 'p');
+            // console.log(p, 'p');
             const { data } = p;
+
+            //chuyễn đỗi kỷ nguyên thành ngày có thể đọc được
+            var myDate = new Date(data.created * 1000);
+            console.log(myDate.toGMTString() + '---' + myDate.toLocaleString());
+
             return (
               <div className="body-api">
                 <div className="post post10">
                   <p>{data.title}</p>
+                  <p id="date">
+                    {myDate.toGMTString() + '---' + myDate.toLocaleString()}
+                  </p>
                   <img src={data.thumbnail} alt="post" />
                   url:<p className="url-color">{data.url}</p>
                 </div>
